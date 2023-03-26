47378825ab08b094b4351a604ec2659f5e316bb4
Chamindu Sauranga <chamindu1567@gmail.com>
2023-03-26 13:00:12 +0530

    getOnesComplement done. Tester to be debugged

---
+++ RV64M_pre/src/test/scala/getOnesComplement_tester.scala
@@ -0,0 +1,29 @@
+import chisel3._
+import chisel3.util._
+import chisel3.Driver
+import chisel3.iotesters._
+
+class getOnesComplement_tester (dut : getOnesComplement) extends PeekPokeTester(dut){
+    poke (dut.io.cin, 0.U)
+    poke (dut.io.i1, 0.U)
+    step (1)
+    expect (dut.io.onesComp, 0.U)
+
+    poke (dut.io.cin, 1.U)
+    poke (dut.io.i1, 0.U)
+    step (1)
+    expect (dut.io.onesComp, "hffffffff".U)
+
+    poke (dut.io.cin, 1.U)
+    poke (dut.io.i1, "hf0f0f0f0f".U)
+    step (1)
+    expect (dut.io.onesComp, "h0f0f0f0f".U)
+
+
+    }
+
+object getOnesComplement_tester extends App{
+    chisel3.iotesters.Driver(()=>new getOnesComplement()) {c =>
+        new getOnesComplement_tester(c)
+    }
+
--- RV64M_pre/xor2.v
+++
@@ -1,9 +0,0 @@
-module xor2(
-  input   clock,
-  input   reset,
-  input   io_a,
-  input   io_b,
-  output  io_out
-);
-  assign io_out = io_a ^ io_b; // @[xor2.scala 14:20]
-endmodule
