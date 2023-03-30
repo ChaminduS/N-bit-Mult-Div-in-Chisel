aaee5165a0ae757247302c4656e1ca8e2726ff04
Chamindu Sauranga <chamindu1567@gmail.com>
2023-03-30 13:20:17 +0530

    Booth substep one step fails

--- RV64M_pre/src/test/scala/booth_substep_tester.scala
+++ RV64M_pre/src/test/scala/booth_substep_tester.scala
@@ -29,10 +29,20 @@ class booth_substep_tester (dut : booth_substep) extends PeekPokeTester(dut){
     poke (dut.io.multiplicand,1.S)
     poke (dut.io.q0,0.U)
     step (1)
-    //println("Result is: " + peek(dut.io.sum).toString)
+    //println("Result is: " + peek(dut.addsub_temp).toString)
     expect (dut.io.next_acc, -1.S)
-    expect (dut.io.next_Q, "h1000000000000000".U)
+    expect (dut.io.next_Q, "h8000000000000000".U)
     expect (dut.io.q0_next, 1.U)
+
+    poke (dut.io.acc, 3.S)
+    poke (dut.io.Q,2.S)
+    poke (dut.io.multiplicand,1.S)
+    poke (dut.io.q0,1.U)
+    step (1)
+    //println("Result is: " + peek(dut.io.sum).toString)
+    expect (dut.io.next_acc, 2.S)
+    expect (dut.io.next_Q, 1.S)
+    expect (dut.io.q0_next, 0.U)
 
     }
 
