42eab65b8b56f8d948835e87f46beae1c02dc1bc
Chamindu Sauranga <chamindu1567@gmail.com>
2023-03-31 09:26:37 +0530

    booth division substep tests passed

--- src/test/scala/booth_div_substep_tester.scala
+++ src/test/scala/booth_div_substep_tester.scala
@@ -3,61 +3,32 @@ import chisel3.util._
 import chisel3.Driver
 import chisel3.iotesters._
 
-class booth_substep_tester (dut : booth_substep) extends PeekPokeTester(dut){
+class booth_div_substep_tester (dut : booth_div_substep) extends PeekPokeTester(dut){
     poke (dut.io.acc, 0.U)
-    poke (dut.io.Q,"hf0f0f0f0f0f0f0f0".U)
-    poke (dut.io.multiplicand,1.U)
-    poke (dut.io.q0,0.U)
+    poke (dut.io.Q,"b0111".U)
+    poke (dut.io.divisor,"b011".U)
     step (1)
-    //println("Result is: " + peek(dut.io.sum).toString)
     expect (dut.io.next_acc, 0.U)
-    expect (dut.io.next_Q, "h7878787878787878".U)
-    expect (dut.io.q0_next, 0.U)
-
-    poke (dut.io.acc, 0.U)
-    poke (dut.io.Q,1.U)
-    poke (dut.io.multiplicand,1.U)
-    poke (dut.io.q0,1.U)
+    expect (dut.io.next_Q, "b1110".U)
+    
+    poke (dut.io.acc, "h00000000000000ff".U) //255<<1 = 510
+    poke (dut.io.Q,"h0000000000000f00".U) //3840<<1 = 7680
+    poke (dut.io.divisor,"h000000000000000f".U) //15
     step (1)
-    //println("Result is: " + peek(dut.io.sum).toString)
-    expect (dut.io.next_acc, 0.U)
-    expect (dut.io.next_Q, 0.U)
-    expect (dut.io.q0_next, 1.U)
+    expect (dut.io.next_acc, 495.U) //510-15 = 495
+    expect (dut.io.next_Q, 7681.U) //7680+1 =7681
 
-    poke (dut.io.acc, 0.S)
-    poke (dut.io.Q,1.S)
-    poke (dut.io.multiplicand,1.S)
-    poke (dut.io.q0,0.U)
+    poke (dut.io.acc, "hf".U) //15<<1 = 30
+    poke (dut.io.Q,"hf0".U) //240<<1 = 480
+    poke (dut.io.divisor,"hff".U) //255
     step (1)
-    //println("Result is: " + peek(dut.as0.io.sum))
-    expect (dut.io.next_acc, -1.S)
-    expect (dut.io.next_Q, "h8000000000000000".U)
-    expect (dut.io.q0_next, 1.U)
-
-    // poke (dut.io.acc, 0.S)
-    // poke (dut.io.Q,-9223372036854775808.S)
-    // poke (dut.io.multiplicand,1.S)
-    // poke (dut.io.q0,0.U)
-    // step (1)
-    // //println("Result is: " + peek(dut.as0.io.sum))
-    // expect (dut.io.next_acc, -1.S)
-    // expect (dut.io.next_Q, -4611686018427387904.S)
-    // expect (dut.io.q0_next, 1.U)
-
-    poke (dut.io.acc, 3.S)
-    poke (dut.io.Q,2.S)
-    poke (dut.io.multiplicand,1.S)
-    poke (dut.io.q0,1.U)
-    step (1)
-    //println("Result is: " + peek(dut.io.sum).toString)
-    expect (dut.io.next_acc, 2.S)
-    expect (dut.io.next_Q, 1.S)
-    expect (dut.io.q0_next, 0.U)
+    expect (dut.io.next_acc, 30.U) //Restored -> 30
+    expect (dut.io.next_Q, 480.U) //480
 
     }
 
-object booth_substep_tester extends App{
-    chisel3.iotesters.Driver(()=>new booth_substep()) {c =>
-        new booth_substep_tester(c)
+object booth_div_substep_tester extends App{
+    chisel3.iotesters.Driver(()=>new booth_div_substep()) {c =>
+        new booth_div_substep_tester(c)
     }
 }
