275c11407ed18fe6f5fb6cff5ea95f53c8ad4a87
Chamindu Sauranga <chamindu1567@gmail.com>
2023-04-03 09:39:35 +0530

    booth div substep adjusted for 32 bit

--- src/main/scala/booth_div_substep.scala
+++ src/main/scala/booth_div_substep.scala
@@ -3,52 +3,52 @@ import chisel3.util._
 
 class booth_div_substep extends Module{
     val io = IO(new Bundle{
-        val acc = Input(SInt(64.W))         //A
-        val Q = Input(SInt(64.W))           
-        val divisor = Input(SInt(64.W))     //M
-        val next_acc = Output(SInt(64.W))
-        val next_Q = Output(SInt(64.W))
+        val acc = Input(SInt(32.W))         //A
+        val Q = Input(SInt(32.W))           
+        val divisor = Input(SInt(32.W))     //M
+        val next_acc = Output(SInt(32.W))
+        val next_Q = Output(SInt(32.W))
     })
 
-    val g1 = Module(new(getOnesComplement))
+    val g1 = Module(new getOnesComplement(32))
 
-    val int_ip = Wire(UInt(64.W))
+    val int_ip = Wire(UInt(32.W))
 
     g1.io.cin := 1.U
     g1.io.i1 := io.divisor.asUInt
     int_ip := g1.io.onesComp
 
     //left shift before sending to the adder
-    val shiftedA = Wire(UInt(65.W))
-    val shiftedQ = Wire(UInt(65.W))
+    val shiftedA = Wire(UInt(33.W))
+    val shiftedQ = Wire(UInt(33.W))
     val shiftedA_LSB = Wire(UInt(1.W))
     val shiftedQ_LSB = Wire(UInt(1.W))
-    val Aout = Wire(UInt(64.W))
+    val Aout = Wire(UInt(32.W))
 
     shiftedA := io.acc.asUInt << 1
-    shiftedA_LSB := io.Q(63)
+    shiftedA_LSB := io.Q(31)
     shiftedQ := io.Q.asUInt << 1
 
-    val as1 = Module(new(addsub_64))
+    val as1 = Module(new(addsub_32))
 
-    val sub_temp = Wire(UInt(64.W))
+    val sub_temp = Wire(UInt(32.W))
 
     as1.io.cin := 1.U
     as1.io.onesComp_ip := int_ip
-    as1.io.i0 := Cat(shiftedA(63,1),shiftedA_LSB)
+    as1.io.i0 := Cat(shiftedA(31,1),shiftedA_LSB)
     sub_temp := as1.io.sum          //sub_temp will hold the value of A-M
 
     //logic loop
-    when (sub_temp(63) === 1.U){
+    when (sub_temp(31) === 1.U){
         shiftedQ_LSB := 0.U 
-        Aout         := Cat(shiftedA(63,1),shiftedA_LSB)
+        Aout         := Cat(shiftedA(31,1),shiftedA_LSB)
     }.otherwise{
         shiftedQ_LSB := 1.U
         Aout         := sub_temp
     }
 
     io.next_acc := Aout.asSInt
-    io.next_Q   := Cat(shiftedQ(63,1),shiftedQ_LSB).asSInt
+    io.next_Q   := Cat(shiftedQ(31,1),shiftedQ_LSB).asSInt
 }
 
 object boothDivSubstep extends App {
--- src/test/scala/booth_div_substep_tester.scala
+++ src/test/scala/booth_div_substep_tester.scala
@@ -11,9 +11,9 @@ class booth_div_substep_tester (dut : booth_div_substep) extends PeekPokeTester(
     expect (dut.io.next_acc, 0.U)
     expect (dut.io.next_Q, "b1110".U)
     
-    poke (dut.io.acc, "h00000000000000ff".U) //255<<1 = 510
-    poke (dut.io.Q,"h0000000000000f00".U) //3840<<1 = 7680
-    poke (dut.io.divisor,"h000000000000000f".U) //15
+    poke (dut.io.acc, "h000000ff".U) //255<<1 = 510
+    poke (dut.io.Q,"h00000f00".U) //3840<<1 = 7680
+    poke (dut.io.divisor,"h0000000f".U) //15
     step (1)
     expect (dut.io.next_acc, 495.U) //510-15 = 495
     expect (dut.io.next_Q, 7681.U) //7680+1 =7681
